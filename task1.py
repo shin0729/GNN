99a5d24d7617991a5f0b162646603099fa01043f
shin0729 <shin.yamaguchi0801@gmail.com>
2024-09-03 13:48:40 +0900

    重み行列Wを固定とする和集約を用いたreadoutを実装しました

--- task1.py
+++ task1.py
@@ -1,17 +1,44 @@
 #行列Wを固定として、与えられたグラフGに対してhGを求めるGNN関数またはクラスを実装
 import numpy as np
-class GNN(self,D,W,T):
+class GNN:
     def __init__(self,D,W,T):
         self.D = D
         self.W = W
         self.T = T
-
-    def relu(self,x):
+    @staticmethod
+    def relu(x):
         return np.maximum(0,x)
     
-    def aggregate1(self,G,x):
+    def f(self,x):
+        return np.frompyfunc(self.relu,1,1)(x)
+
+    def aggregate(self,G,x):
         a = np.dot(G,x)
         return a
     
-    def aggregate2(self , a):
-        x = self.frompyfunc()
+    def conbine(self , a):
+        h_v = self.f(np.dot(a,self.W))
+        return h_v
+    
+    def readout(self,G):
+        h_G = np.zeros((G.shape[0],self.D))
+        h_G = np.random.rand(G.shape[0],self.D)
+        for _ in range(self.T):
+            a = self.aggregate(G,h_G)
+            h_G = self.conbine(a)
+        return h_G
+
+#main関数
+if __name__ == "__main__":
+    G = np.array([[0,0,1,0,0],
+                  [0,0,1,0,0],
+                  [1,1,0,1,1],
+                  [0,0,1,0,1],
+                  [0,0,1,1,0]])
+    D = 3
+    W = np.random.rand(D,D)
+    print(W)
+    T = 2
+    gnn = GNN(D,W,T)
+    h_G = gnn.readout(G)
+    print(h_G)
